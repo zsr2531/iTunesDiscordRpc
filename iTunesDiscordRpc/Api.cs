9f85d3afd475c270e6b52a34b32d388a34a22cab
zsr2531 <warhaamer1337@gmail.com>
2020-12-05 13:43:11 +0100

    Add C# bindings

---
+++ iTunesDiscordRpc/Api.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+namespace iTunesDiscordRpc
+{
+    public static class Api
+    {
+        [DllImport("itunes.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool Initialize([MarshalAs(UnmanagedType.Bool)] bool needsCoInit = false);
+
+        [DllImport("itunes.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnInitialize([MarshalAs(UnmanagedType.Bool)] bool needsCoUnInit = false);
+
+        [DllImport("itunes.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCurrentTrackLength([MarshalAs(UnmanagedType.I8)] ref long seconds);
+
+        [DllImport("itunes.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCurrentPosition([MarshalAs(UnmanagedType.I8)] ref long seconds);
+
+        [DllImport("itunes.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCurrentTrackName([MarshalAs(UnmanagedType.BStr)] ref string name);
+
+        [DllImport("itunes.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCurrentTrackAuthor([MarshalAs(UnmanagedType.BStr)] ref string name);
+
+        [DllImport("itunes.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCurrentTrackAlbum([MarshalAs(UnmanagedType.BStr)] ref string name);
+    }
+}
